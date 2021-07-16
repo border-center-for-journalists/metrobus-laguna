ff5a607c5212e000a8ac90c469d8bbdc98d9293c
El Sonny <sonny@spaceshiplabs.com>
2021-07-16 13:56:56 +0200

    downloadable contracts

--- src/Contracts.svelte
+++ src/Contracts.svelte
@@ -1,18 +1,26 @@
 <script>
 	export let contracts;
+	const _contracts = contracts.map(c => {
+
+	});
 </script>
 <main>
 	{#each contracts as contract}
-	<a href='' class='article'>
-		<nav>
-			<i class='material-icons'>download</i>
-		</nav>
-		<content>
-			<p class='title'>{contract.concept}</p>
-			<p class='company'>{contract.company}</p>
-			<p class='ammount'>{contract.ammount}</p>
-		</content>
-	</a>
+		<article class:downloadable={contract.filename}>
+			{#if contract.filename}
+				<a href='contracts/{contract.filename}' target='_blank' class='article downloadable'></a>
+			{/if}
+			<nav>
+				{#if contract.filename}
+					<i class='material-icons'>download</i>
+				{/if}
+			</nav>
+			<content>
+				<p class='company'>{contract.company}</p>
+				<p class='title'>{contract.concept}</p>
+				<p class='ammount'>{contract.ammount}</p>
+			</content>
+		</article>
 	{/each}
 </main>
 <style>
@@ -22,7 +30,7 @@
 		max-width: 1150px;
 		margin: 0 auto;
 	}
-	.article{
+	article{
 		display: flex;
 		color: white;
 		padding: 8px 15px;
@@ -30,25 +38,44 @@
 		border-radius: 5px;
 		margin: 25px 25px 0 0;
 		flex: 0 1 calc(33% - 49px);
+		position: relative;
 	}
-	.article:nth-child(3n){
+	article a{
+		display: block;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	article.downloadable{
+		cursor: pointer;
+	}
+	article.downloadable:hover{
+		color: black;
+		background-color: white;
+		transition: all 300ms ease;
+		border: 3px solid #878787;
+	}
+	article:nth-child(3n){
 		margin-right: 0;
 	}
-	.article nav{
+	article nav{
 		padding: 5px 7px 0 5px;
 	}
-	.article nav i{
+	article nav i{
 		font-size: 24px;
 	}
 	.title{
-		font-size: 12px;
-		margin:  0 auto;
+		font-size: 13px;
+		line-height: 15px;
+		margin:  10px auto;
 	}
 	.company{
 		font-size: 17px;
-		line-height: 28px;
+		line-height: 25px;
 		text-transform: uppercase;
-		margin: 5px 0;
+		margin: 0 0 5px 0;
 	}
 	.ammount{
 		font-size:  17px;
